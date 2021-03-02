3e8c28c950b0e8cacbc9faa7ba62dfc1e6f4b6c5
Marcelo Chiaradia <chiaradiamarcelo@gmail.com>
2021-03-02 16:41:15 +0100

    Update scripts

--- tableau-server-GUI-tests/set_up_scripts/execute_script_remotely.sh
+++ tableau-server-GUI-tests/set_up_scripts/execute_script_remotely.sh
@@ -4,7 +4,7 @@
 
 execute_set_up_script_on_remote_server() {
     echo "# Executing script in remote server"
-    ssh $1@$2 python < $3 -
+    ssh $1@$2 'bash -s' < $3
 }
 
 execute_set_up_script_on_remote_server $1 $2 $3
--- tableau-server-GUI-tests/set_up_scripts/package_connector.sh
+++ tableau-server-GUI-tests/set_up_scripts/package_connector.sh
@@ -70,9 +70,9 @@ remove_tableau_connector_plugin_sdk_directory () {
 }
 
 remove_directory_if_exists () {
-    if [ -d "$1" ]; then
-        rm -rf $1
-    fi
+  if [ -d "$1" ]; then
+    rm -rf $1
+  fi
 }
     
 set_up_environment
