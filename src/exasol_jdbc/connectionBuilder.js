d18e9b4a12ac7cff4d190792b1051b589a73cd8f
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-10-05 11:35:02 +0000

    Adapt to Exasol

--- src/exasol_jdbc/connectionBuilder.js
+++ src/exasol_jdbc/connectionBuilder.js
@@ -1,5 +1,12 @@
 (function dsbuilder(attr) {
-    var urlBuilder = "jdbc:exa://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?";
+    // See https://docs.exasol.com/connect_exasol/drivers/jdbc.htm
+    const url = "jdbc:exa:"
+        + attr[connectionHelper.attributeServer] + ":"
+        + attr[connectionHelper.attributePort]
+        + ";validateservercertificate=0"
+        + ";feedbackinterval=1"
+        + ";clientname=TableauConnector"
+        + ";clientversion=ver";
 
-    return [urlBuilder];
+    return [url];
 })
