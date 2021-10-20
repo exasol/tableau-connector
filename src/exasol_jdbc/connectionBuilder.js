7a824a7781318ff4157e6b8a8b80b796f48c7198
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-10-20 13:16:12 +0000

    Build JDBC url with fingerprint and certificate validation arguments

--- src/exasol_jdbc/connectionBuilder.js
+++ src/exasol_jdbc/connectionBuilder.js
@@ -1,14 +1,28 @@
 (function dsbuilder(attr) {
+
+    const authentication = attr[connectionHelper.attributeAuthentication];
+    const fingerprint = attr["v-fingerprint"];
+    const validateServerCertificate = attr["v-validateservercertificate"];
+    
+    logging.Log("connectionBuilder.js: input args: authentication='" + authentication
+        + "', fingerprint='" + fingerprint
+        + "', validateServerCertificate='"+ validateServerCertificate+"'");
+
+    var fingerprintArg = "";
+    if(fingerprint && fingerprint.trim().length > 0) {
+        fingerprintArg = "/" + fingerprint.trim();
+    }
+
     // See https://docs.exasol.com/connect_exasol/drivers/jdbc.htm
     const url = "jdbc:exa:"
         + attr[connectionHelper.attributeServer]
-        //+ "/" + attr["fingerprint"]
+        + fingerprintArg
         + ":"
         + attr[connectionHelper.attributePort]
-        + ";validateservercertificate=0"
+        + ";validateservercertificate=" + validateServerCertificate
+        + ";encryption=1"
         + ";feedbackinterval=1"
-        + ";clientname=TableauConnector"
-        + ";clientversion=ver";
+        + ";clientname=TableauDesktop";
 
     return [url];
 })
--- src/exasol_jdbc/connectionProperties.js
+++ src/exasol_jdbc/connectionProperties.js
@@ -1,10 +1,7 @@
 (function propertiesbuilder(attr) {
+    logging.Log("Running connectionProperties.js")
     var props = {};
     props["user"] = attr[connectionHelper.attributeUsername];
     props["password"] = attr[connectionHelper.attributePassword];
-
-    props["fingerprint"] = attr['v-fingerprint'];
-    props["validateservercertificate"] = attr['v-validateservercertificate'];
-
     return props;
 })
