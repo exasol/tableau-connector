5c9c57b8864e3f94c00bdecb8ddabeb8734a8385
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-10-29 11:58:23 +0200

    Add support for Kerberos on Tableau Server

--- src/exasol_jdbc/connectionProperties.js
+++ src/exasol_jdbc/connectionProperties.js
@@ -3,6 +3,10 @@
     function log(str) {
         logging.Log("connectionProperties.js: " + str)
     }
+    function isEmpty(str) {
+        return (!str || 0 === str.length);
+    }
+
     const authentication = attr[connectionHelper.attributeAuthentication];
 
     var props = {};
@@ -10,10 +14,19 @@
     props["password"] = attr[connectionHelper.attributePassword];
 
     if (authentication === 'auth-integrated') {
-        logging.Log("connectionProperties.js: Using integrated auth")
-        props["gsslib"] = "gssapi";
-        props["jaasLogin"] = "false";
-        props["jaasApplicationName"] = "com.sun.security.jgss.krb5.initiate";
+        // if attributeTableauServerUser is non-empty, it means the connector plugin is currently being accessed in a Tableau Server environment
+        var serverUser = attr[connectionHelper.attributeTableauServerUser];
+        if (!isEmpty(serverUser)) {
+            log("Running on Server using integrated auth with user '" + serverUser + "'")
+            props["user"] = serverUser;
+            props["gsslib"] = "gssapi";
+            props["jaasLogin"] = "false";
+        } else {
+            log("Running on Desktop using integrated auth")
+            props["gsslib"] = "gssapi";
+            props["jaasLogin"] = "false";
+            props["jaasApplicationName"] = "com.sun.security.jgss.krb5.initiate";
+        }
     } else {
         log("Using non-integrated auth '" + authentication + "'")
     }
