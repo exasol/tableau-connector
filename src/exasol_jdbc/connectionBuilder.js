36a1cb53fddb072fbe7268be0c6f479fe5742100
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-10-29 15:17:14 +0200

    Cleanup connector script

--- src/exasol_jdbc/connectionBuilder.js
+++ src/exasol_jdbc/connectionBuilder.js
@@ -1,9 +1,16 @@
 (function dsbuilder(attr) {
+    const jdbcDriverDebugEnabled = false;
+    const jdbcDriverLogDir = "C:\\tmp";
 
     function log(str) {
         logging.Log("connectionBuilder.js: " + str)
     }
 
+    function isEmpty(str) {
+        return (!str || 0 === str.trim().length);
+    }
+
+    const hostName = attr[connectionHelper.attributeServer];
     const authentication = attr[connectionHelper.attributeAuthentication];
     const fingerprint = attr["v-fingerprint"];
     const validateServerCertificate = attr["v-validateservercertificate"];
@@ -12,14 +19,11 @@
         + "', fingerprint='" + fingerprint
         + "', validateServerCertificate='" + validateServerCertificate + "'");
 
-    var fingerprintArg = "";
-    if (fingerprint && fingerprint.trim().length > 0) {
-        fingerprintArg = "/" + fingerprint.trim();
-    }
-
+    const fingerprintArg = !isEmpty(fingerprint) ? ("/" + fingerprint.trim()) : "";
+    const debugArg = jdbcDriverDebugEnabled ? (";debug=1;logdir=" + jdbcDriverLogDir) : "";
     // See https://docs.exasol.com/connect_exasol/drivers/jdbc.htm
     const url = "jdbc:exa:"
-        + attr[connectionHelper.attributeServer]
+        + hostName
         + fingerprintArg
         + ":"
         + attr[connectionHelper.attributePort]
@@ -28,10 +32,9 @@
         + ";clientname=TableauDesktop"
         // Required to activate Kerberos authentication
         // https://www.exasol.com/support/browse/SUPPORT-26947
-        + ";kerberoshostname=" + attr[connectionHelper.attributeServer]
+        + ";kerberoshostname=" + hostName
         //+ ";kerberosservicename=exasol"
-        //+ ";debug=1;logdir=C:\\tmp"
-        + "";
+        + debugArg;
     log("JDBC URL: '" + url + "'");
     return [url];
 })
