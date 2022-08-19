92dad5d71bbd4842aeca914f5c3d49e6f23598d7
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-08-19 14:29:45 +0200

    Code cleanup

--- src/exasol_jdbc/connectionBuilder.js
+++ src/exasol_jdbc/connectionBuilder.js
@@ -1,36 +1,27 @@
-const { hostname } = require("os");
-
 (function dsbuilder(attr) {
     "use strict";
-    const kerberosServiceName = "exasol";
 
     function isEmpty(str) {
         return (!str || 0 === str.trim().length);
     }
 
-
-
-
     function getHostName() {
         const hostName = attr[connectionHelper.attributeServer];
         if (isEmpty(hostName) || isEmpty(hostName.trim())) {
-            return ""
+            return "";
         }
-        return hostName.trim()
+        return hostName.trim();
     }
 
     function getPort() {
         const port = attr[connectionHelper.attributePort];
-        if (isEmpty(port)||isEmpty(port.trim())) {
+        if (isEmpty(port) || isEmpty(port.trim())) {
             return "";
         }
-        return ":"+ port.trim();
+        return ":" + port.trim();
     }
 
-
     // See https://docs.exasol.com/connect_exasol/drivers/jdbc.htm
-    const url = "jdbc:exa:"
-        + getHostName()
-        + getPort();
+    const url = "jdbc:exa:" + getHostName() + getPort();
     return [url];
 })
--- src/exasol_jdbc/connectionProperties.js
+++ src/exasol_jdbc/connectionProperties.js
@@ -5,9 +5,6 @@
         return (!str || 0 === str.length);
     }
 
-    const authentication = attr[connectionHelper.attributeAuthentication];
-    const serverAuthMode = attr[connectionHelper.attributeTableauServerAuthMode];
-
     function getServerUser() {
         return attr[connectionHelper.attributeTableauServerUser];
     }
@@ -16,35 +13,35 @@
         return !isEmpty(getServerUser());
     }
 
-
     function useKerberos() {
-        return attr[connectionHelper.attributeAuthentication] === connectionHelper.valueAuthIntegrated
+        return attr[connectionHelper.attributeAuthentication] === connectionHelper.valueAuthIntegrated;
     }
 
     function getValidateServerCertificate() {
         const value = attr["v-validateservercertificate"];
         if (value === 0 || value === "0") {
-            return "0"
+            return "0";
         } else {
-            return "1"
+            return "1";
         }
     }
 
     function getCertificateFingerprint() {
         const fingerprint = attr["v-fingerprint"];
         if (isEmpty(fingerprint) || isEmpty(fingerprint.trim())) {
-            return undefined
+            return undefined;
         }
 
-        return fingerprint.trim()
+        return fingerprint.trim();
     }
 
+    // See https://docs.exasol.com/db/latest/connect_exasol/drivers/jdbc.htm#SupportedDriverProperties
     const props = {};
 
     if (useKerberos()) {
-        const hostName = attr[connectionHelper.attributeServer]
-        props["kerberoshostname"] = hostName
-        props["kerberosservicename"] = "exasol"
+        const hostName = attr[connectionHelper.attributeServer];
+        props["kerberoshostname"] = hostName;
+        props["kerberosservicename"] = "exasol";
     }
 
     if (runningOnServer()) {
